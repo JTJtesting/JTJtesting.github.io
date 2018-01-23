ce184ba0a9f7e092addf75df266a0eafd95a0443
darkdude2222 <darkdude2222@gmail.com>
2018-01-23 06:00:51 -0500

    commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var slider = document.getElementById("Range1");
+var output = document.getElementById("ValRange1");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+    output.innerHTML = this.value;
+}
