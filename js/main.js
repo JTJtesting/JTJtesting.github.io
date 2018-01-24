0649e37c0f9a5a092430588b9be1dc6ede95fdce
darkdude2222 <darkdude2222@gmail.com>
2018-01-24 08:44:54 -0500

    demo

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var Caesarverschuifing;
+
 
 var slider = document.getElementById("Range1");
 var output = document.getElementById("ValRange1");
 output.innerHTML = slider.value; // Display the default slider value
-
+var Caesarverschuifing = slider.value;
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
     output.innerHTML = this.value;
