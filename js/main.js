bcf63fc0db11ec325ba8cf83a235835021d432f0
darkdude2222 <darkdude2222@gmail.com>
2018-01-24 07:47:48 -0500

    demo

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var Ceasarverschuifing;
+var Caesarverschuifing;
 
 var slider = document.getElementById("Range1");
 var output = document.getElementById("ValRange1");
@@ -8,19 +8,19 @@ output.innerHTML = slider.value; // Display the default slider value
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
     output.innerHTML = this.value;
-    Ceasarverschuifing = this.value;
+    Caesarverschuifing = this.value;
 }
 
-function Ceasarcijfer(input, verschuif){
-  var Ceasaroutput = ""
+function Caesarcijfer(input, verschuif){
+  var Caesaroutput = ""
   var array = new array();
   for(var i=0; i<input.length; i++){
-    Ceasaroutput += alphabet[array.push((alphabet.indexOf(input[i])+verschuif)%25)];
+    Caesaroutput += alphabet[array.push((alphabet.indexOf(input[i])+verschuif)%25)];
   }
   
 }
 
-$('#Ceasarcijfer').each(function() {
+$('#Caesarcijfer').each(function() {
    var elem = $(this);
 
    // Save current value of element
@@ -33,7 +33,7 @@ $('#Ceasarcijfer').each(function() {
        // Updated stored value
        elem.data('oldVal', elem.val());
        elem.val(elem.data('oldVal').toLowerCase());
-       $("#Ceasarouput").text(Ceasarcijfer(elem.val(),Ceasarverschuifing));
+       $("#Caesarouput").text(Caesarcijfer(elem.val(),Caesarverschuifing));
      }
    });
  });
